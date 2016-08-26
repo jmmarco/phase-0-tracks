318c1a13de6346005ada8e35354beec32bf8c205
Juan Martin Marco <jmmarco@gmail.com>
2016-08-26 19:59:39 -0300

    Add questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,14 @@
+# Gather data
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.to_i
+puts "What year were you born?"
+yob = gets.to_i
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+garlic_bread = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?"
+insurance = gets.chomp
