d08484320b7d6c09d2c93cc9f99e70d3861e82a3
Juan Martin Marco <jmmarco@gmail.com>
2016-08-28 16:05:53 -0300

    Improve comments

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,11 +1,12 @@
 # Students: Vince Roti, Juan Marco
 
 # Pseudocode
-# Encryption: Take a string and iterate through each letter (spaces included) and advance one a letter.
+# Encryption: Take a string and iterate through each letter (spaces included) and advance one a letter
 
 def encrypt(word)
     index = 0
     result = ""
+    # Iterate through each letter in the word
     while index < word.length
         # If that letter happens to be an edge case
         if word[index] == "z"
@@ -16,22 +17,23 @@ def encrypt(word)
         end
     index += 1
     end
+    # Print the encrypted word
     p result
 end
 
 # Pseudocode
-# Decryption: Take a string, iteraste throuch each letter (spaces include) and go back one letter.
+# Decryption: Take a string, iterate throuch each letter (spaces included) and go back one letter
 
 def decrypt(word)
     result = ""
     alphabet = "abcdefghijklmnopqrstuvwxyz"
-    # Make the word and array
+    # Make the word and array.
     word = word.split("")
 
-    # Iterate through each letter
+    # Iterate through each letter in the array.
     for letter in word do
         index = 0
-        # For a single letter iterate through the alphabet and decrpyt it
+        # For a given letter, iterate through the alphabet and decrypt each one
         while index < alphabet.length
             if letter == alphabet[index]
                 result += alphabet[index -1]
@@ -44,10 +46,13 @@ def decrypt(word)
         index += 1
         end
     end
+    # Print the decrypted word
     p result
 end
 
-encrypt("abc") # should return "bcd"
-encrypt("zed") # should return "afe"
-decrypt("bcd") # should return "abc"
-decrypt("afe") # should return "zed"
+# encrypt("abc") # should return "bcd"
+# encrypt("zed") # should return "afe"
+# decrypt("bcd") # should return "abc"
+# decrypt("afe") # should return "zed"
+
+# decrypt(encrypt("swordfish"))
