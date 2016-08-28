12b90401725877a00868f754fbdb3c6c295d76ac
Juan Martin Marco <jmmarco@gmail.com>
2016-08-28 13:15:10 -0300

    Decrypt code not working properly

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -4,22 +4,37 @@
 # Encryption: Take a string and iterate through each letter (spaces included) and advance one a letter.
 
 def encrypt(word)
-	index = 0
-	result = ""
-	while index < word.length
-		result += word[index].next
-		index += 1
-	end
-	p result
+  index = 0
+  result = ""
+  while index < word.length
+    result += word[index].next
+    index += 1
+  end
+  p result
 end
-
-
 
 # Pseudocode
 # Decryption: Take a string, iteraste throuch each letter (spaces include) and go back one letter.
 
+def decrypt(word)
+  index = 0
+  alphabet = "abcdefghijklmnopqrstuvwxyz"
+  result = ""
+  while index < alphabet.length
+    if word[index] == "a"
+      result += "z"
+    elsif word[index] == "z"
+      result += "a"
+    elsif word[index] == alphabet[index]
+      result += alphabet[index - 1]
+    end
+    p "word[index] is:#{word[index]}, and alphabet[index] is: #{alphabet[index]}"
+    index += 1
+  end
+  p result
+end
 
-encrypt("abc") # should return "bcd"
+#encrypt("abc") # should return "bcd"
 # encrypt("zed") # should return "afe"
-# decrypt("bcd") # should return "abc"
-# decrypt("afe") # should return "zed"
+decrypt("bcd") # should return "abc"
+#decrypt("afe") # should return "zed"
