4d5a6e45fd70f158dfe4d9c177ca53c1ed8031ee
Juan Martin Marco <jmmarco@gmail.com>
2016-08-28 15:53:42 -0300

    Finally working

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -4,44 +4,50 @@
 # Encryption: Take a string and iterate through each letter (spaces included) and advance one a letter.
 
 def encrypt(word)
-  index = 0
-  result = ""
-  while index < word.length
-    # For each letter go forward one character
-    result += word[index].next
+    index = 0
+    result = ""
+    while index < word.length
+        # If that letter happens to be an edge case
+        if word[index] == "z"
+            result += "a"
+        # For each letter go forward one character
+        else
+            result += word[index].next
+        end
     index += 1
-  end
-  p result
+    end
+    p result
 end
 
 # Pseudocode
 # Decryption: Take a string, iteraste throuch each letter (spaces include) and go back one letter.
 
 def decrypt(word)
-  result = ""
-  alphabet = "abcdefghijklmnopqrstuvwxyz"
-  # Make the word and array
-  word = word.split("")
-  # Check if letter is in the alphabet
-  for letter in word do
-      index = 0
-      puts "current letter is: #{letter}"
-      while index < alphabet.length
-        if letter == alphabet[index]
-          result += alphabet[index -1]
-        elsif letter == "a"
-          result = "z"
-        elsif letter == "z"
-          result = "a"
-        end
+    result = ""
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    # Make the word and array
+    word = word.split("")
+
+    # Iterate through each letter
+    for letter in word do
+        index = 0
+        # For a single letter iterate through the alphabet and decrpyt it
+        while index < alphabet.length
+            if letter == alphabet[index]
+                result += alphabet[index -1]
+            # Take into account all edge cases
+            elsif letter == "a"
+                result = "z"
+            elsif letter == "z"
+                result = "a"
+            end
         index += 1
-      end
-  end
-  p result
+        end
+    end
+    p result
 end
 
-  #encrypt("abc") # should return "bcd"
-  # encrypt("zed") # should return "afe"
-  #decrypt("bcd") # should return "abc"
-  decrypt("afe") # should return "zed"
-  decrypt("efg") # should return "def"
+encrypt("abc") # should return "bcd"
+encrypt("zed") # should return "afe"
+decrypt("bcd") # should return "abc"
+decrypt("afe") # should return "zed"
