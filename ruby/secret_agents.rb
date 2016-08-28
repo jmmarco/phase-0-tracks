170234fd3122608ed8c27163a86204ca7e42c168
Juan Martin Marco <jmmarco@gmail.com>
2016-08-28 12:47:28 -0300

    Add encryption algorithm

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -3,12 +3,23 @@
 # Pseudocode
 # Encryption: Take a string and iterate through each letter (spaces included) and advance one a letter.
 
+def encrypt(word)
+	index = 0
+	result = ""
+	while index < word.length
+		result += word[index].next
+		index += 1
+	end
+	p result
+end
+
+
 
 # Pseudocode
 # Decryption: Take a string, iteraste throuch each letter (spaces include) and go back one letter.
 
 
-# encrypt("abc") # should return "bcd"
+encrypt("abc") # should return "bcd"
 # encrypt("zed") # should return "afe"
 # decrypt("bcd") # should return "abc"
 # decrypt("afe") # should return "zed"
