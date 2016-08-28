776d31b5d5015c3d82cb91552b50faa3e71e0913
Juan Martin Marco <jmmarco@gmail.com>
2016-08-28 12:29:37 -0300

    Add pseudocode and students

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,14 @@
+# Students: Vince Roti, Juan Marco
+
+# Pseudocode
+# Encryption: Take a string and iterate through each letter (spaces included) and advance one a letter.
+
+
+# Pseudocode
+# Decryption: Take a string, iteraste throuch each letter (spaces include) and go back one letter.
+
+
+# encrypt("abc") # should return "bcd"
+# encrypt("zed") # should return "afe"
+# decrypt("bcd") # should return "abc"
+# decrypt("afe") # should return "zed"
