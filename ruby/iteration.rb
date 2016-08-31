3c40fb3c7a2fa193bf408e4ce59c51802d974748
Juan Martin Marco <jmmarco@gmail.com>
2016-08-31 10:38:52 -0300

    Add array and hash

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -6,3 +6,17 @@ def favorite_food
 end
 
 favorite_food { |name1, name2| puts "#{name2} likes Spaghetti and #{name1} likes Tacos!" }
+
+
+# Release 1 - Data Structures
+
+cereal = ["Lucky Charms", "Cap'n Crunch", "Fruty Pebbles", "Honey Combs"]
+
+pets = {
+	# Animal and breed
+	"cat" => "calico",
+	"dog" => "border collie"
+	"dog" => "french bulldog"
+}
+
+
