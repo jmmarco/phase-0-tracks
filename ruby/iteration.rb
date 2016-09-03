53d5c9f18f419ce1278fb744e5b29dc7e3acee98
Juan Martin Marco <jmmarco@gmail.com>
2016-09-03 17:05:49 -0300

    Add select_if method

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -15,12 +15,14 @@ favorite_food { |name1, name2| puts "#{name2} likes Spaghetti and #{name1} likes
 # Add Data Structures
 
 # Array of integers
-numbers = [1, 2, 12, 50, 87, 99]
+numbers = [1, 2, 7, 12, 50, 87, 99]
 
 # A hash for pets
 pets = {
 	"cat" => 1,
-	"dog" => 2
+	"dog" => 2,
+	"fish" => 7,
+	"tiger" => 3
 }
 
 # Release 1: Use .each, .map, and .map!
@@ -47,18 +49,24 @@ puts "After .map! #{numbers}"
 
 # Release 2: Use the Documentation
 
+# Method 1: .delete_if
+
 puts "Before .delete_if: #{numbers}"
-
-# Get rid of numbers greater than 50
-numbers.delete_if {|num| num > 50 }
-
+numbers.delete_if {|num| num > 50 } # Get rid of numbers greater than 50
 puts "After .delete_if: #{numbers}"
 
 puts "Before .delete_if: #{pets}"
-
-# Get rid of cats(s)
-pets.delete_if {|pet| pet == "cat"}
-
+pets.delete_if {|pet| pet == "cat"} # Get rid of cats(s) ^_^
 puts "Before .delete_if: #{pets}"
 
+
+# Method 2: .select
+
+puts "Before .select: #{numbers}"
+numbers.select! {|num| num <= 10 } # Select numbers less than or equal to 10 use ! to modify original array
+puts "After .select #{numbers}"
+
+puts "Before .select #{pets}"
+pets.select! {|pet, quantity| quantity <= 5 } # Select pet(s) that have 5 or less
+puts "After .select #{pets}"
 
