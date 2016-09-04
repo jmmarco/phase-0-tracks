7d492db6b77fc2646b1b2f26df4775d4635ffb2a
Juan Martin Marco <jmmarco@gmail.com>
2016-09-04 17:36:35 -0300

    Add pseudocode and user menu

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,17 +1,21 @@
-# Enter details of client
-# Name, age, number of children, decor theme, payment method,
+# Greet the user, ask what does he want to do
+# display client list, add client, modify client, remove client
+# Name, age, number of children, decor theme, payment method
 
+user_input = nil
+until user_input == 5
+    puts "Welcome to the client database."
+    puts "Available options are:"
+    puts "1. Display client list"
+    puts "2. Add client"
+    puts "3. Modify client"
+    puts "4. Remove Client"
+    puts "5. Quit"
 
-loop do
-	puts "Enter customer name:"
-	input = gets.chomp
-	break if input == 'done'
+    user_input = gets.chomp.to_i
+    clients = [] # Initialize an array to place each client
+
 end
 
-client = {
-	:name => '',
-	:age => ''.to_i,
-	:num_of_children => ''.to_i,
-	:decor_theme => '',
-	:payment_method => ''
-}
+
+
