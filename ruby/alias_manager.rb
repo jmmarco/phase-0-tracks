04585761b69a40adcda50654c8a9ab496abaae4b
Juan Martin Marco <jmmarco@gmail.com>
2016-09-05 15:01:29 -0300

    Add pseudocode and define method

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,15 @@
+# Grab an agent's name, split it into two words
+
+def alias_generator(name)
+	arr = name.split(' ')
+	p name
+	p arr
+end
+
+alias_generator("Steve Marco")
+
+
+
+
+# Swap the first and last name
+# For each vowel inside of each name, swap it for the next vowel
