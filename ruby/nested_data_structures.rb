462b4d7a20a15c7a22205306e1a7cb4943338ba5
Juan Martin Marco <jmmarco@gmail.com>
2016-09-05 13:00:11 -0300

    Access data structure with some methods

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -10,3 +10,15 @@ refrigerator = {
 	# Main compartment (inside there's a green box to hold fresh produce)
 	main: ['milk', 'cheese', 'ham', 'eggs', green_box: ['lettuce', 'tomatoes', 'carrots']]
 }
+
+# How many ice cubes?
+puts "The freezer has #{refrigerator[:freezer][:ice_box]} ice cubes!"
+
+# Get some ice cream
+puts "We have some delicious #{refrigerator[:freezer][:main][0]} in the freezer!"
+
+# Brag about the awesome tomatoes!
+puts "Today i went to the market and bought some wonderful #{refrigerator[:main][4][:green_box][1]}!"
+
+# Make a sandwich
+puts "I'm gonna go make me a #{refrigerator[:main][2]} and #{refrigerator[:main][1]} sandwich."
