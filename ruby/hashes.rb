6c78a15b0645b6fd89e07ebd6505f59e6ef7c5bc
Juan Martin Marco <jmmarco@gmail.com>
2016-09-05 09:54:39 -0300

    Allow user to re-enter information

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -45,12 +45,62 @@ loop do
         client["Colors"] << input
     end
 
-
     puts "Enter the customer theme:"
     client["Theme"] = gets.chomp
 
     puts "Enter the preferred payment method:"
     client["Payment Method"] = gets.chomp
+
+    # Present the information to the Interior Designer
+    puts "Here's the customer's details:"
+    pp client
+
+    puts "Is the info ok (y/n)?"
+    input = gets.chomp.downcase
+    if input == 'n'
+        until input == "done"
+            puts "Ok, What field would you like to change?. Enter 'done' when finished."
+            input = gets.chomp
+            case input
+            when "name"
+                puts "Ok, enter a customer name:"
+                client["Name"] = gets.chomp
+            when "age"
+                puts "Ok, enter an age:"
+                client["Age"] = gets.chomp.to_i
+            when "children"
+                puts "Ok, does the customer have children? (y/n)"
+                input = gets.chomp.downcase
+                if  input == "y"
+                    client["Children"] = true
+                else
+                    client["Children"] = false
+                end
+            when "color"
+                # Empty the "Colors" array
+                client["Colors"] = []
+                loop do
+                    puts "Enter another color and press 'enter'. Type 'done' when completed." if client["Colors"].length > 0
+                    puts "Enter a color, and press 'enter'. Type 'done' when completed."
+                    input = gets.chomp
+                    break if input == "done"
+                    client["Colors"] << input
+                end
+            when "theme"
+                puts "Ok, enter a customer theme:"
+                client["Theme"] = gets.chomp
+            when "payment"
+                puts "Ok, enter the preferred payment method:"
+                client["Payment Method"] = gets.chomp
+            end # End of case loop
+
+            # Present the updated information
+            puts "Here's the updated customer's details: #{client}"
+        end # End of until loop
+    else
+        puts "We're done here. Bye!"
+        break
+    end # End of conditional
 
     # Present the client information
     puts "Here's the customer's you entered details:"
