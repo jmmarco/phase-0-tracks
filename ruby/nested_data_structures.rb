d57aef5ab581b54a139a1abd31a3b24891bbf1f5
Juan Martin Marco <jmmarco@gmail.com>
2016-09-05 12:59:38 -0300

    Add nested data structure

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,12 @@
+# Nested Data Structure
+# Real-world object: Refrigerator
+
+refrigerator = {
+	# Freezer compartment
+	freezer: {
+		main: ['ice cream', 'green peas'],
+		ice_box: 35 # And ice box to hold ice cubes (I like to keep track of every single ice cube)
+	},
+	# Main compartment (inside there's a green box to hold fresh produce)
+	main: ['milk', 'cheese', 'ham', 'eggs', green_box: ['lettuce', 'tomatoes', 'carrots']]
+}
