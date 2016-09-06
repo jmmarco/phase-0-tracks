bc83c588528fc205e1c8fd542317b286f704328a
Juan Martin Marco <jmmarco@gmail.com>
2016-09-06 17:55:39 -0300

    Add behavior to class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,35 @@
 class Puppy
+	# Fetch
+    def fetch(toy)
+        puts "I brought back the #{toy}!"
+        toy
+    end
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
-  end
+    # Speak
+    def speak(num)
+        num.times {puts "Woof!"}
+    end
 
+    # Rollover
+    def roll_over
+        puts "*rolls over*"
+    end
+
+    # Calculate age
+    def dog_years(human_years)
+        human_years * 5
+    end
+
+    # Say hi with paw
+    def paw
+        puts "*Say's hi with the paw*"
+    end
 end
 
+# Driver Code
+max = Puppy.new
+max.fetch("ball")
+max.speak(4)
+max.roll_over
+puts "Max is: #{max.dog_years(2)} years old!"
+max.paw
