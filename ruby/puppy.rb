71ed6593937e76311562e1b664cc5c8abfcce27c
Juan Martin Marco <jmmarco@gmail.com>
2016-09-06 16:35:01 -0300

    Add class design

---
+++ ruby/puppy.rb
@@ -0,0 +1,12 @@
+# SPECIES ----------------------------------
+# Canis familiaris
+#
+# CHARACTERISTICS --------------------------
+# Lifespan: 10 years
+# Guardian: true
+#
+# BEHAVIOR ---------------------------------
+# Friendly
+# Loyal
+# Eats without chewing
+
