b13831f4ed259ae1e3fd975e97da9523c9213140
Juan Martin Marco <jmmarco@gmail.com>
2016-09-10 18:54:07 -0300

    Add method to update items and check quantities

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -42,46 +42,68 @@ end
 # Method to add items
 def add_item(cart,item, quantity)
     cart[item] = quantity
-    puts "Here's your current cart #{cart}"
     cart
 end
 
 # Method to remove items
 def remove_item(cart, item)
-	cart.delete(item)
-	cart
+    cart.delete(item)
+    cart
 end
+
+# Method to update items
+def update_item(cart, item, quantity)
+    cart[item] = quantity
+    cart
+end
+
+# Method to check for valid quantities
+def check_quantity(quantity)
+    begin
+        quantity = Integer(quantity)
+    rescue ArgumentError, TypeError
+        puts "Thats not a number. Exiting to main menu.."
+    end
+    quantity
+end
+
 
 # Driver code
 puts "Welcome to the shopping cart 2000(TM)"
+cart = {}
+
 
 loop do
     puts "To get started, please enter some items to the cart using spaces"
     options = ['add', 'remove', 'update', 'display']
     puts "Available options are: #{options}"
     input = gets.chomp.downcase
-    cart = {}
 
     case input
     when "add"
         puts "Enter the item you wish to add:"
         item = gets.chomp
         puts "How many of #{item} do you want?"
-        quantity = gets.chomp
-        begin
-            quantity = Integer(quantity)
-            add_item(cart,item, quantity)
-            puts "Ok, added #{quantity} #{item}"
-        rescue ArgumentError, TypeError
-            puts "Thats not a number. Try again."
-        end
+        check_quantity(gets.chomp)
+        puts "Ok, added #{quantity} #{item}"
     when "remove"
         puts "Enter the item you wish to remove:"
         item = gets.chomp
-
+        remove_item(cart, item)
+        puts "Ok, removed #{item} from cart."
+    when "update"
+        puts "Here's your current cart:"
+        cart.each do |item, quantity|
+            puts "#{quantity} #{item}"
+        end
+        puts "What item do you wish to udpate?"
+        input = gets.chomp
+        puts "Ok, now enter the new quantity"
+       	check_quantity(gets.chomp)
+        update_item(cart, item, quantity)
+        puts "Ok, I updadated to #{quantity} #{item}(s)"
     else
         puts "Not a valid option. Try again."
     end
-
 
 end
