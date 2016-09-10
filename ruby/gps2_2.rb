6c847a44f18fa7c0da92b997b57b74dfea6de7a4
Juan Martin Marco <jmmarco@gmail.com>
2016-09-10 13:58:53 -0300

    Add driver code to get user input

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -1,9 +1,9 @@
 # Method to create a list
 # input: string of items separated by spaces (example: "carrots apples cereal pizza")
 # steps:
-  # Add desired item(s)
-  # set default quantity to 1 for each item
-  # print the list to the console (print has using .each)
+# Add desired item(s)
+# set default quantity to 1 for each item
+# print the list to the console (print has using .each)
 # output:
 
 # Method to add an item to a list
@@ -28,14 +28,24 @@
 
 # Define the cart that accepts items as a parameter
 def shopping_cart(items)
-	cart = {}
-	array = items.split(" ")
+    cart = {}
+    array = items.split(" ")
 
-	array.each do |item|
-		cart[item] = 1
-		puts "Adding #{item}.. to the cart"
-	end
-	cart
+    array.each do |item|
+        cart[item] = 1
+        puts "Adding #{item}.. to the cart"
+    end
+    puts "Here's your current cart #{cart}"
+    cart
 end
 
-puts shopping_cart("carrots apples cereal pizza")
+# Driver code
+puts "Welcome to the shopping cart 2000(TM)"
+
+loop do
+    puts "To get started, please enter some items to the cart using spaces"
+    puts "Like this: carrots tomatoes cereal - To exit type 'done'"
+    items = gets.chomp.downcase
+    break if items == 'done'
+    shopping_cart(items)
+end
