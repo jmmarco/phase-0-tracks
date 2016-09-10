5a4aa648fa1c8d09e7def8aaec571d240ebca045
Juan Martin Marco <jmmarco@gmail.com>
2016-09-10 13:45:58 -0300

    Define and write methods to handle input and output

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -1,27 +1,27 @@
 # Method to create a list
 # input: string of items separated by spaces (example: "carrots apples cereal pizza")
 # steps:
-  # [fill in any steps here]
-  # set default quantity
-  # print the list to the console [can you use one of your other methods here?]
-# output: [what data type goes here, array or hash?]
+  # Add desired item(s)
+  # set default quantity to 1 for each item
+  # print the list to the console (print has using .each)
+# output:
 
 # Method to add an item to a list
 # input: item name and optional quantity
-# steps:
-# output:
+# steps: prompt user for item and quanity
+# output: print a message to confirm and display updated cart
 
 # Method to remove an item from the list
-# input:
-# steps:
-# output:
+# input: item name (quanity not necessary)
+# steps: delete target item
+# output: print a message to confirm and display updated cart
 
 # Method to update the quantity of an item
-# input:
-# steps:
-# output:
+# input: enter target item
+# steps: display item name and quantity
+# output: print a message to confirm and display updated cart
 
 # Method to print a list and make it look pretty
-# input:
-# steps:
-# output:
+# input: shopping cart (hash)
+# steps: iterate through each item in hash and print the key and values
+# output: print entire cart (hash)
