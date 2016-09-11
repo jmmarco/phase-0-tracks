4af190f91f597e242c38dedecb3c182efcc6f49c
Juan Martin Marco <jmmarco@gmail.com>
2016-09-11 18:29:37 -0300

    Add program to create santas

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,68 +1,63 @@
 # Define Santa class
 class Santa
-	attr_accessor :name, :gender, :ethnicity
-	# But you usually use attr_accessor for both read and write
+    attr_accessor :name, :gender, :ethnicity, :age
+    # But you usually use attr_accessor for both read and write
 
-	def initialize(name, gender, ethnicity)
-		puts "Initializing Santa instance ..."
-		@name = name
-		@gender = gender
-		@ethnicity =  ethnicity
-		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-		@age = 0
-	end
+    def initialize(gender, ethnicity)
+        puts "Initializing Santa instance ..."
+        @name = name
+        @gender = gender
+        @ethnicity =  ethnicity
+        @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+        @age = 0
+    end
 
-	# Make Santa speak
-	def speak
-		puts "Ho, ho, ho! Haaaappy holidays!"
-	end
+    # Make Santa speak
+    def speak
+        puts "Ho, ho, ho! Haaaappy holidays!"
+    end
 
-	# Have some milk and cookies
-	def eat_milk_and_cookies(cookie)
-		puts "That was a good #{cookie}!"
-	end
+    # Have some milk and cookies
+    def eat_milk_and_cookies(cookie)
+        puts "That was a good #{cookie}!"
+    end
 
-	# Setter methods (make data writable)
-	def name=(new_name)
-		@name = new_name
-	end
+    # Give santa an age between 0 and 140
+    def age
+    	age = Random.new
+    	age = age.rand(0..140)
+    end
 
-	def gender=(new_gender)
-		@gender = new_gender
-	end
-
-	def ethnicity=(new_ethnicity)
-		@ethnicity = new_ethnicity
-	end
 end
 
 # Driver code
 puts "Welcome to the Santa Generator 4000(TM)"
-santas = []
+
+
+genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+
+
+puts "Let's make some Santa's for the world!"
+puts "How many santas do you want to create?"
+num = ""
 loop do
-	options = ['add', 'remove', 'update', 'quit']
-	puts "Let's make some Santa's for the world!"
-	puts "Options are: #{options}"
-	input = gets.chomp.downcase
-	break if input == "quit"
-
-	case input
-	when "add"
-		puts "Enter a name for this santa:"
-		name = gets.chomp
-		puts "Enter a gender for this santa:"
-		gender = gets.chomp
-		puts "Enter an ethnicity for this santa:"
-		ethnicity = gets.chomp
-		santas << Santa.new(name, gender, ethnicity)
-	when "update"
-
-	else
-		puts "Thats not a valid option. Try again."
-	end
+    puts "Enter a number (please use integers only):"
+    num = Integer(gets) rescue false
+    break if num.is_a? Integer
+    puts "Not a valid number. Try again"
 end
 
-# Display info
-p santas[0].gender
-p santas[0].name
-p santas[0].ethnicity
+# Create a new array to hold all the santas
+santas = []
+
+num.times do
+	santas << Santa.new(genders.sample, ethnicities.sample)
+end
+
+# Loop over each santa and and output a random gender, ethnicity and age
+santas.each do |s|
+	puts "Here's your santa:"
+	puts "It's a #{s.gender} and #{s.ethnicity}"
+	puts "And has an age of: #{s.age}"
+end
