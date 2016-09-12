e3c4bbe184284ebf7a03a4bb8bf8a414e9815377
Juan Martin Marco <jmmarco@gmail.com>
2016-09-11 23:35:58 -0300

    Convert a standalone module to a mixin

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,15 +1,44 @@
+# INITIAL CODE
+#
 # Create a shout module
+# module Shout
+# 	def self.yell_angrily(words)
+# 		words + "!!!" + " :("
+# 	end
+
+# 	def self.yelling_happily(words)
+# 		words + "!!!" + " :D"
+# 	end
+# end
+
+# # Driver code
+# puts Shout.yell_angrily("what the hell")
+# puts Shout.yelling_happily("I am Awesome")
+
+# UPDATED CODE
 module Shout
-	def self.yell_angrily(words)
-		words + "!!!" + " :("
+	# Exlirated method
+	def exilirated(assigment)
+		puts "I'm soooo happpy! I figured out Ruby #{assigment} !!!"
 	end
 
-	def self.yelling_happily(words)
-		words + "!!!" + " :D"
+	# Frustrated method
+	def frustrated(assigment)
+		puts "I'm stuck and frustrated with Ruby #{assigment}. I'm going for a walk!"
 	end
 end
 
-# Driver code
-puts Shout.yell_angrily("what the hell")
+class Joey
+	include Shout
+end
 
-puts Shout.yelling_happily("I am Awesome")
+class Peter
+	include Shout
+end
+
+# Driver code
+joey = Joey.new
+joey.frustrated("Modules")
+
+peter = Peter.new
+peter.exilirated("Classes and Modules")
