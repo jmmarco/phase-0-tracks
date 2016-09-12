89879886c97c277c4a75b3554f12102328c877f1
Juan Martin Marco <jmmarco@gmail.com>
2016-09-12 03:44:53 -0300

    Pinpoint words and store them

--- ruby/game/word_game.rb
+++ ruby/game/word_game.rb
@@ -1,21 +1,40 @@
 # Game Class
 
 class WordGame
-	attr_accessor :attempts
+	attr_accessor :attempts, :word_length, :guessed, :word_to_guess
 
 
 	def initialize
 		puts "Intializing word game ..."
-
+		@attempts = 0
+		@guessed = []
+		@word_to_guess = []
+		@is_over = false
 	end
 
 	# Method to check word
-	def check_word(word)
-		w = word.chars
-		w.each do |l|
-			puts "Letter: #{l}"
+	def prepare_word(word)
+		@word_to_guess = word.chars
+		g = []
+		@word_to_guess.each do |l|
+			g << " - "
 		end
+		@guessed = g
+		@attempts = word.length
 	end
+
+	def check_letter(letter)
+		if @word_to_guess.include? letter
+			puts "You found: #{letter}!"
+			@guessed[@word_to_guess.index(letter)] = letter
+		else
+			puts "Sorry, #{letter} is not part of the word."
+		end
+		@attempts -= 1
+		puts "You have #{@attempts} attempts remaining."
+	end
+
+
 
 end
 
@@ -29,7 +48,17 @@ puts "This a two players game. User #1 will choose and write it down so that Use
 puts "User #1, please enter a word to be guessed:"
 word = gets.chomp
 
-game.check_word(word)
+game.prepare_word(word)
+
+
+loop do
+	puts "User #2, please enter a word to be guessed:"
+	game.check_letter(gets.chomp)
+	output = game.guessed
+	puts "The guessed array is #{output}"
+	break if game.attempts == 0
+end
+
 
 
 
