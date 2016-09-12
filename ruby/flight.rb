b07ac636577838e97b18624781c313352636bf8d
Juan Martin Marco <jmmarco@gmail.com>
2016-09-11 23:05:47 -0300

    Add base classes and flight module

---
+++ ruby/flight.rb
@@ -0,0 +1,15 @@
+# Bird Class
+class Bird
+
+end
+
+# Plane Class
+class Plane
+
+end
+
+module Flight
+	def take_off(altitude)
+		puts "Taking off and ascending until reaching #{altitude} ..."
+	end
+end
