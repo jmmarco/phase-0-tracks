8c035e0d0f7abb32d0894592d756373b942979c5
Juan Martin Marco <jmmarco@gmail.com>
2016-09-11 23:07:41 -0300

    Add driver code

--- ruby/flight.rb
+++ ruby/flight.rb
@@ -1,11 +1,11 @@
 # Bird Class
 class Bird
-
+	inlcude Flight
 end
 
 # Plane Class
 class Plane
-
+	include Flight
 end
 
 module Flight
@@ -13,3 +13,11 @@ module Flight
 		puts "Taking off and ascending until reaching #{altitude} ..."
 	end
 end
+
+
+# Driver code
+bird = Bird.new
+bird.take_off(800)
+
+plane = Plane.new
+plane.take_off(30000)
