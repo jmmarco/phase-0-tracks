c7c83d77ec15b68ad1e7fd8dd718096345cc1d99
Juan Martin Marco <jmmarco@gmail.com>
2016-09-14 20:20:56 -0300

    Push items into both arrays

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,3 +1,11 @@
 colors = ["blue", "red", "green", "yellow"];
 
 names = ["Peter", "Brian", "Glenn", "Charly"];
+
+// Add additional color to the colors array
+colors.push("Black");
+
+// Add addtional name
+names.push("Stewie");
+
+console.log(colors, names);
