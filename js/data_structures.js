617a2f95e87e425205c018e0a7dcc0095415a6be
Juan Martin Marco <jmmarco@gmail.com>
2016-09-14 20:53:46 -0300

    Add loop to incorporate names and colors into object

--- js/data_structures.js
+++ js/data_structures.js
@@ -3,9 +3,18 @@ colors = ["blue", "red", "green", "yellow"];
 names = ["Peter", "Brian", "Glenn", "Charly"];
 
 // Add additional color to the colors array
-colors.push("Black");
+colors.push("black");
 
 // Add addtional name
 names.push("Stewie");
 
-console.log(colors, names);
+// console.log(colors, names);
+
+
+var horses = {};
+
+for (var i = 0; i < names.length; i++) {
+  horses[names[i]] = colors[i];
+}
+
+console.log(horses);
