4703a6f59eebf9cbe8c1824d3a6629564f15708e
Juan Martin Marco <jmmarco@gmail.com>
2016-09-13 23:53:04 -0300

    Reverse a string

---
+++ js/explore.js
@@ -0,0 +1,21 @@
+/*
+Create an empty array to store the revered string
+Loop through each character in the string starting from the end(be sure to substract one,
+javascript counts from zero). Push each string into the array. Output to the console and return the reversed string.
+*/
+
+function reverse(string) {
+	var reversed = [];
+
+	for (var i = string.length -1; i >= 0 ; i--) {
+		reversed.push(string[i]);
+	}
+
+	// Output to the console
+	console.log(reversed.join(""));
+	// Return the reversed string
+	return reversed.join("");
+}
+
+// Call the function with an example string
+reverse("hello");
