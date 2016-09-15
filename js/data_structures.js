b5581cb3120b6fbeb22f509c95b10126832ecd4e
Juan Martin Marco <jmmarco@gmail.com>
2016-09-14 21:20:34 -0300

    Add car constructor

--- js/data_structures.js
+++ js/data_structures.js
@@ -24,3 +24,17 @@ for (var key in horses) {
 
 // console.log(horses);
 
+//  Car constructor
+
+function  Car(engine, wheels, seats) {
+  this.engine = engine;
+  this.wheels = wheels;
+  this.seats = seats;
+
+  this.goFast = function() { console.log("** ACELERATES TO HYPERSPEED **"); };
+}
+
+my_car = new Car("electric", 4, 5);
+console.log(my_car);
+
+my_car.goFast();
