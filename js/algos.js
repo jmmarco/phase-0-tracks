ee1a940518e2a7df739345f750a7c6277d0f2f24
Juan Martin Marco <jmmarco@gmail.com>
2016-09-16 21:15:48 -0300

    Add pseudocode for release 0

---
+++ js/algos.js
@@ -0,0 +1,8 @@
+/*
+Release 0: Find the Longest Phrase
+- Grab an array of phrases
+- Loop through each phrase:
+  - If the length of the current phrase is greater than the previous
+  - Store the length of it in a 'result' variable
+- Return the result variable
+*/
