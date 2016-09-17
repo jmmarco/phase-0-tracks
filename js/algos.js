b3b90c9129b04728d48d214bfb767ed628832e63
Juan Martin Marco <jmmarco@gmail.com>
2016-09-17 20:30:20 -0300

    Add pseudocode for release 2

--- js/algos.js
+++ js/algos.js
@@ -13,20 +13,20 @@ Release 0: Find the Longest Phrase
 
 function longestPhrase(phrases) {
 
-    var longestSoFar = 0,
-        result;
+  var longestSoFar = 0,
+    result;
 
-    phrases.forEach(function(phrase) {
-        if (phrase.length > longestSoFar) {
-            longestSoFar = phrase.length;
-            result = phrase;
-        }
+  phrases.forEach(function(phrase) {
+    if (phrase.length > longestSozFar) {
+      longestSoFar = phrase.length;
+      result = phrase;
+    }
 
-    });
+  });
 
-    console.log("The longest phrase is:", result);
-    console.log("And it contains a total of: " + result.length + " characters!");
-    return result;
+  console.log("The longest phrase is:", result);
+  console.log("And it contains a total of: " + result.length + " characters!");
+  return result;
 }
 
 // Driver code
@@ -38,12 +38,12 @@ Release 1: Find a Key-Value Match
 - Grab two objects
 - Loop through each key and value of the first object
   - if the key and value match any of the keys and values in the second object, return true, otherwise return false
-*/
+  */
 
 function compareKeyValue(obj1, obj2) {
   // for..in loops are a great way to iterate through objects in JS
   for (var key in obj1) {
-    // Check if the key-value pair of the first object mataches the one on the second object
+    // Check if the key-value pair of the first object matches the one on the second object
     if ((key in obj2) && (obj1[key] == obj2[key]))
       return true;
   }
@@ -52,9 +52,13 @@ function compareKeyValue(obj1, obj2) {
 }
 
 // Driver code
-a = compareKeyValue({ name: "Steven", age: 54 }, { name: "Tamir", age: 54 });
-b = compareKeyValue({ type: "Car", engine: "diesel" }, { type: "Car", engine: "petrol" });
+// a = compareKeyValue({ name: "Steven", age: 54 }, { name: "Tamir", age: 54 });
+// b = compareKeyValue({ type: "Car", engine: "diesel" }, { type: "Car", engine: "petrol" });
+// console.log("a is:", a);
+// console.log("b is:", b);
 
-console.log("a is:", a);
-console.log("b is:", b);
-console.log("b is:", c);
+/*
+Release 2: Generate Random Test Data
+Take an integer to calculate the length and build an array of strings of the given length.
+Generate random words of varying length with a minimum of 1 letter and a maximum of 10 letters.
+*/
