b29943651dcd75581ad2ce938572825a15852496
Juan Martin Marco <jmmarco@gmail.com>
2016-09-26 00:24:09 -0300

    prevent table from being created again

--- databases/kitten_maker/my_kitten_maker.rb
+++ databases/kitten_maker/my_kitten_maker.rb
@@ -8,10 +8,18 @@ require 'faker'
 
 # Create SQLite3 database
 db = SQLite3::Database.new("kittens.db")
+
 # Learn about fancy string delimeters
+create_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS kittens(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
+    age INT
+  )
+SQL
 
 # create kittens table (if it's not there already)
-
+db.execute(create_table_cmd)
 # add a test kitten
 
 # explore ORM by retrieving data
