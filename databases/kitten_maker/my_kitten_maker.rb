817e19bb2cd68c1b4933f4bbcef5f307d0972092
Juan Martin Marco <jmmarco@gmail.com>
2016-09-26 00:07:17 -0300

    Setup initial kitten file

---
+++ databases/kitten_maker/my_kitten_maker.rb
@@ -0,0 +1,19 @@
+# OPERATION KITTEN EXPLOSION
+
+# Talk about Object Relational Mapping (ORM)
+
+# Require gems
+
+# Create SQLite3 database
+
+# Learn about fancy string delimeters
+
+# create kittens table (if it's not there already)
+
+# add a test kitten
+
+# explore ORM by retrieving data
+
+# Add looootssss of kittens!
+# so. many. kittens.
+#KittenExplosion
