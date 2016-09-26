8166dcbf9deee38a0b90bad0d655f8dae33788b1
Juan Martin Marco <jmmarco@gmail.com>
2016-09-26 13:04:06 -0300

    Add driver code

--- databases/todo-app/todo.rb
+++ databases/todo-app/todo.rb
@@ -39,3 +39,31 @@ def show_items(db)
     puts "You added: #{item} on #{date}. This item has an id of: #{id}"
   end
 end
+
+# Driver code
+
+# Generate a date
+date = Date.today.strftime("%m/%d/%y")
+# Welcome user
+puts "Welcome to the Todoenator 4000(TM)"
+
+# Present options
+loop do
+  puts "What would you like to do?"
+  puts "Available options are: 'show', 'add', 'remove' or 'done'."
+  input = gets.chomp.downcase
+  break if input == 'done'
+  if input == 'show'
+    show_items(db)
+  elsif input == 'add'
+    puts "Ok, enter something to do:"
+    item = gets.chomp
+    create_entry(db, item, date)
+  elsif input == 'remove'
+    puts "Ok, enter the id of the item you wish to remove:"
+    id = gets.chomp.to_i
+    remove_entry(db, id)
+  else
+    puts "You entered an invalid option. Try again."
+  end
+end
